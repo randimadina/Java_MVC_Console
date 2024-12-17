86fed5e6fe8d9b896f20e7deadea60208a97607e
randimadina <randimadina@gmail.com>
2024-12-18 00:22:18 +0530

    Initial Project 20241218

---
+++ Customer.java
@@ -0,0 +1,31 @@
+public class Customer{
+    private int Id;
+    private String Title;
+    private String FirstName;
+    private String LastName;
+    private String Phone;
+    private String Email;
+
+    public Customer(int id, String title, String firstname, String lastname, String phone, String email){
+        this.Id = id;
+        this.Title = title;
+        this.FirstName = firstname;
+        this.LastName = lastname;
+        this.Phone = phone;
+        this.Email = email;
+    }
+
+    public void SetId(int id)   {   this.Id = id;}
+    public void SetTitle(String title)    { this.Title = title;}
+    public void SetFirstName(String firstname)    { this.FirstName = firstname;}
+    public void SetLastName(String lastname)    { this.LastName = lastname;}
+    public void SetPhone(String phone)    { this.Phone = phone;}
+    public void SetEmail(String email)    { this.Email = email;}
+
+    public int GetId()  {   return Id;}
+    public String GetTitle() {   return Title;}
+    public String GetFirstName() {   return FirstName;}
+    public String GetLastName() {   return LastName;}
+    public String GetPhone() {   return Phone;}
+    public String GetEmail() {   return Email;}
+}
---
+++ mainapp.java
@@ -0,0 +1,9 @@
+//  Author  :   Randima Dinalankara
+//  Date    :   18/12/2024
+//  File    :   Class file that holds the main function
+
+public class mainapp{
+    public static void main(String[] argc){
+        
+    }
+}
